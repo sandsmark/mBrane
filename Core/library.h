d3a06044ed75b3f02aee0e556ffa9eb2e61b727e
eric@ru.is <eric@ru.is@6c7a44ae-e94a-0410-89a4-9db84d9dd2fd>
2010-05-07 14:23:58 +0000

    Refactoring 4

--- Core/library.h
+++ Core/library.h
@@ -131,7 +131,7 @@ public:	\
 const	char	*C::ClassName=#C;	\
 extern	"C"{	\
 	mBrane::sdk::module::_Module *	cdecl	New##C(){	return	new	C();	}	\
-	const	mBrane::uint16	C##_CID(){	return	C::CID();	}	\
+	const	core::uint16	C##_CID(){	return	C::CID();	}	\
 }
 
 
